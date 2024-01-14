a39dab05b743212c09f7b41afb31f0deeaea33ec
wpc <wpc68@outlook.com>
2024-01-14 20:27:11 +0800

    初始化

---
+++ daocao_study/daocao_starter/src/main/java/com/wpc/starter/DaoCaoApplication.java
@@ -0,0 +1,15 @@
+package com.wpc.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wpc
+ * @date 2024/1/14 19:29
+ */
+@SpringBootApplication
+public class DaoCaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DaoCaoApplication.class,args);
+    }
+}
